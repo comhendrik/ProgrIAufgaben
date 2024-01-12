66f29ff737e48a549f8bf6b91956f1a4f0a1e5fa
comhendrik <76622601+comhendrik@users.noreply.github.com>
2024-01-12 10:25:45 +0100

    Neue Klasse

--- Aufgabenzettel01/src/Aufgaben.java
+++ Aufgabenzettel01/src/Aufgaben.java
@@ -2,18 +2,7 @@ import Prog1Tools.IOTools;
 
 public class Aufgaben {
     public void aufgabeZwei() {
-        System.out.println("Aufgabe 2");
-        int i,l,m,n;
-        l = 8/3;
-        i = l * 6 * l + 1; // i =25
-        l = m = n = l * 4; // l = m = n = 8
-        n = n*m; // n = 64
-        m = n/l; // m = 8
-        l = i * m +n; // l = 264
-        System.out.println("i = " + i);
-        System.out.println("l = " + l);
-        System.out.println("m = " + m);
-        System.out.println("n = " + n);
+
     }
     public void aufgabeDrei() {
         System.out.println("Aufgabe 3");
